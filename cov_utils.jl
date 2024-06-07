e8003ea889b2ad0f317c99d9d7794803f7e0ad01
Ana Sofia Mendez Uzsoy <auzsoy@login20.chn.perlmutter.nersc.gov>
2024-06-07 13:29:19 -0700

    added new methods & used formatter

--- cov_utils.jl
+++ cov_utils.jl
@@ -3,7 +3,7 @@
 Parameters
 ----------
 spec : :class:Matrix
-    DESI spectra in a matrix of size (num_wavelengths, n) where n is an arbitrary number (but usually num_spec or num_good_spec)
+    spectra in a matrix of size (num_wavelengths, n) where n is number of spectra
 Csky : :class:Matrix
     sky covariance matrix of size (num_wavelengths, num_wavelengths)
 Cres : :class:Matrix, optional
@@ -16,14 +16,59 @@ res : :class:Matrix
 μskyA : :class:Matrix
     The residual component of spec, size (num_wavelengths, n
 """
-function apply_Csky_no_split(spec,Csky,Cres)
-    Ctotinv = inv(cholesky((Csky+Cres)))
+function apply_Csky_no_split(spec, Csky, Cres)
+    Ctotinv = inv(cholesky((Csky + Cres)))
 
     # infer the sky and residual components
-    μsky = (Csky*Ctotinv)*spec
-    res = (Cres*Ctotinv)*spec
+    μsky = (Csky * Ctotinv) * spec
+    res = (Cres * Ctotinv) * spec
 
     return res, μsky
 end
 
 
+"""Creates mask where 1 if value is not nan, else 0
+
+Parameters
+----------
+x : :class:Matrix
+    Matrix of floats
+Returns
+-------
+Matrix y, of same shape as x, with 0s where x has nans and 1s otherwise
+"""
+function nan_mask(x)
+    # want 1 if value is not nan, else 0
+    y = copy(x)
+    for i in eachindex(x)
+        @inbounds y[i] = ifelse(isnan(x[i]), 0, 1)
+    end
+    return y
+end
+
+
+"""Makes data-driven wavelength-wavelength covariance matrix from spectra
+
+Parameters
+----------
+spec : :class:Matrix
+    spectra in a matrix of size (num_wavelengths, n) where n is number of spectra
+counts_matrix : :class:Bool
+    if true, divide by symmetric matrix (num_wavelengths, num_wavelengths) with entries consisting of the number of spectra with non-nan values for that wavelength bin
+    if false, will just use the number of spectra for each wavelength
+
+Returns
+-------
+covariance matrix of size (num_wavelengths, num_wavelengths)
+"""
+function make_cov(spec; counts_matrix = true)
+    spec_to_use = spec
+    if sum(isnan.(spec)) > 0 # if there are nans, set them to zero
+        spec_to_use = nan_to_zero(copy(spec))
+    end
+    if counts_matrix
+        good_mask = nan_mask(spec)
+        return (spec_to_use * spec_to_use') ./ (good_mask * good_mask')
+    end
+    return (spec * spec') ./ size(spec, 2)
+end
--- spectrum_utils.jl
+++ spectrum_utils.jl
@@ -15,10 +15,10 @@ Returns
 Log spaced spectrum (flux values)
 """
 
-function linear_to_log(spec, wave; real_log_range=log_wave_range, kernelsize = 4)
-    itp = Interpolations.interpolate(spec, Interpolations.Lanczos(kernelsize));
-    etp = Interpolations.extrapolate(itp,0) # fill value is zero
-    
+function linear_to_log(spec, wave; real_log_range = log_wave_range, kernelsize = 4)
+    itp = Interpolations.interpolate(spec, Interpolations.Lanczos(kernelsize))
+    etp = Interpolations.extrapolate(itp, 0) # fill value is zero
+
     # these are the wavelength values of log spaced bins
     real_linear_log_range = 10 .^ (real_log_range)
 
@@ -27,7 +27,7 @@ function linear_to_log(spec, wave; real_log_range=log_wave_range, kernelsize = 4
     index_range = ((real_linear_log_range .- minimum(wave)) / (wave[2] - wave[1])) .+ 1 # because one indexed
 
     log_binned_spectrum = etp(index_range)
-    
+
     return log_binned_spectrum
 end
 
@@ -42,7 +42,7 @@ Returns
 Wavelength of Lyman alpha emission shifted to given redshift, in Angstroms
 """
 function get_wavelength(z)
-   return 1215.67*(1+z) 
+    return 1215.67 * (1 + z)
 end
 
 """Get log10-wavelength of Lyman alpha line in Angstroms
@@ -53,10 +53,10 @@ z : :class:float
     redshift to get LAE wavelength at
 Returns
 -------
-Log10-wavelengths of Lyman alpha emission shifted to given redshift, in Angstroms
+Log10-wavelength of Lyman alpha emission shifted to given redshift, in log(Angstroms)
 """
 function get_log_wavelength(z)
-   return log10.(get_wavelength(z))
+    return log10.(get_wavelength(z))
 end
 
 """Shift a spectrum to be at a given redshift
@@ -80,9 +80,9 @@ function adjust_spectrum_log(spectrum, z; ref_z = 2.45)
     if pixel_shift > 0
         adjusted_spectrum[1:pixel_shift] .= NaN
     else
-        adjusted_spectrum[(end + pixel_shift):end] .= NaN
+        adjusted_spectrum[(end+pixel_shift):end] .= NaN
     end
-    return adjusted_spectrum    
+    return adjusted_spectrum
 end
 
 """Shift a spectrum to be at a given redshift and also shift
@@ -100,10 +100,10 @@ ref_z : :class:float
     redshift to shift spectrum to
 Returns
 -------
-Log10-wavelengths of Lyman alpha emission shifted to given redshift, in Angstroms
+Spectrum and ivars shifted to be at ref_z using the same log-wavelength bins
 """
-function adjust_spectrum_and_ivar(spectrum, ivar, z)
-    adjustment = log10(1 + 2.45) - log10(1 + z)
+function adjust_spectrum_and_ivar(spectrum, ivar, z; ref_z = 2.45)
+    adjustment = log10(1 + ref_z) - log10(1 + z)
     pixel_shift = round(Int, adjustment / delLog)
     adjusted_spectrum = ShiftedArrays.circshift(spectrum, pixel_shift)
     adjusted_ivar = ShiftedArrays.circshift(ivar, pixel_shift)
@@ -111,26 +111,56 @@ function adjust_spectrum_and_ivar(spectrum, ivar, z)
         adjusted_spectrum[1:pixel_shift] .= NaN
         adjusted_ivar[1:pixel_shift] .= NaN
     else
-        adjusted_spectrum[(end + pixel_shift):end] .= NaN
-        adjusted_ivar[(end + pixel_shift):end] .= NaN
+        adjusted_spectrum[(end+pixel_shift):end] .= NaN
+        adjusted_ivar[(end+pixel_shift):end] .= NaN
     end
     return adjusted_spectrum, adjusted_ivar
 end
 
-function nan_mask(x)
-    # want 1 if value is not nan, else 0
-    y = copy(x)
-    for i in eachindex(x)
-        @inbounds y[i] = ifelse(isnan(x[i]), 0, 1)
-    end
-    return y
-end
+"""In-place replacement of nans with zeros in a matrix
 
-# https://stackoverflow.com/questions/53463436/how-to-replace-inf-values-with-nan-in-array-in-julia-1-0
+Based on https://stackoverflow.com/questions/53463436/how-to-replace-inf-values-with-nan-in-array-in-julia-1-0
+
+Parameters
+----------
+x : :class:Matrix
+    Matrix of floats
+Returns
+-------
+nothing
+"""
 function nan_to_zero(x)
     # replaces NaNs with zeros in an array
     # modifies the actual array so be careful!
     for i in eachindex(x)
         @inbounds x[i] = ifelse(isnan(x[i]), 0, x[i])
     end
+end
+
+"""Shift a spectrum to be at a given redshift and also shift
+    the ivars in the same way
+
+Parameters
+----------
+spectrum : :class:Vector
+    log-binned spectrum at redshift z
+ivar : :class:Vector
+    log-binned ivar corresponding to spectrum
+z : :class:float
+    true redshift of spectrum
+ref_z : :class:float
+    redshift to shift spectrum to
+Returns
+-------
+Spectrum and ivars shifted to be at ref_z using the same log-wavelength bins
+"""
+function filter_nanvar(spec; nanvar_threshold = 10^-1, zero_out_nans = true)
+    nanvars = nanvar(spec, dims = 1)
+    nanvar_mask = vec(((nanvars .< nanvar_threshold) .&& (nanvars .> 0)))
+
+    filtered_spec = copy(spec[:, nanvar_mask])
+    if zero_out_nans
+        nan_to_zero(filtered_spec)
+    end
+    return filtered_spec
 end
