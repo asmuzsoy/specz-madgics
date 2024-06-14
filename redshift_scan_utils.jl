add61314229ba5c793f0de42dd1e78c8d8db6634
Ana Sofia Mendez Uzsoy <auzsoy@login03.chn.perlmutter.nersc.gov>
2024-06-14 11:40:17 -0700

    added docstring to redshift scan methods

--- redshift_scan_utils.jl
+++ redshift_scan_utils.jl
@@ -1,4 +1,23 @@
-# Here we shift a spectrum that is at z=2.45 to an arbitrary z
+"""Shift a spectrum template which is currently at default_z to an arbitrary z, including sub-pixel shifts
+
+Parameters
+----------
+V : :class:Matrix
+    spectrum template in a matrix of size (10, num_wavelength_bins + padding * 2, num_eigen vectors)
+z : :class:float
+    redshift to shift to
+default_z : :class:float, optional
+    redshift that the template is currently at, defaults to 2.45
+padding : :class:int, optional
+    amount of zero-padding on either end of the template, defaults to 1000 wavelength bins
+
+Returns
+-------
+adjusted_V : :class:Matrix
+    Adjusted spectrum template, size (num_wavelength_bins, num_eigenvectors)
+pixel_shift : :class:int
+    The number of pixels to shift required to shift to the given redshift
+"""
 function adjust_V(V, z; default_z = 2.45, padding=1000)
     adjustment = log10(1 + z) - log10(1 + default_z)
     
@@ -16,8 +35,29 @@ function adjust_V(V, z; default_z = 2.45, padding=1000)
     return adjusted_V[padding+1:end - padding,:], pixel_shift
 end
 
+"""Determines optimal spectroscopic redshift for any number of spectra given template
 
-function scan(spec, padded_Vmat, wave_range; z_to_test = new_z_to_test)
+Parameters
+----------
+spec : :class:Matrix
+    Matrix of spectram size (num_wavelength_bins, num_spectra)
+padded_Vmat : :class:Matrix
+    spectrum template in a matrix of size (10, num_wavelength_bins + padding * 2, num_eigen vectors)
+wave_range : :class:vector
+    range of indexes of the total wavelength bins that the template is, i.e. if they are the same it will be 1:num_wavelength_bins
+z_to_test : :class:vector
+    array of redshifts to try
+
+Returns
+-------
+chisq : :class:Matrix
+    Matrix of chisq surfaces, of size (num_spectra, length(z_to_test))
+min_chisqs : :class:vector
+    Vector of best delta-chisq values for each spectrum, size (num_spectra,)
+new_zs : :class:vector
+    Vector of best-fit redshifts for each spectrum, size (num_spectra,)
+"""
+function scan(spec, padded_Vmat, wave_range, z_to_test)
     small_Cinv = Cinv[wave_range, wave_range]
 
     numspec = size(spec)[2]
